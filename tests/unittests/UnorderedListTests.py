826d674cb3aea5d61f50bfc7410aeee23def33bd
Brett Parker <iDunno@sommitrealweird.co.uk>
2007-12-31 02:56:28 +0000

    * Add unit test for some of the badly formed lists that we get after the   feedparser "sanitizer" has a word with the HTML (currently fails)

--- tests/unittests/UnorderedListTests.py
+++ tests/unittests/UnorderedListTests.py
@@ -25,5 +25,23 @@ class UnorderedListTests(unittest.TestCase):
         output = parser.gettext()
         self.assertEqual(output, expectedoutput)
 
+    def testbadlyformed(self):
+        try:
+            from rss2maildir import HTML2Text
+        except:
+            sys.path.append(os.path.sep.join(self.inputpath.split(os.path.sep)[0:-1]))
+            try:
+                from rss2maildir import HTML2Text
+            except:
+                self.assert_(False)
+
+        input_path = os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[0:-1])
+        input = open(os.path.join(input_path, "html", "unorderedlist-badlyformed.html")).read()
+        expectedoutput = open(os.path.join(input_path, "expected", "unorderedlist-badlyformed.txt")).read()
+        parser = HTML2Text()
+        parser.feed(input)
+        output = parser.gettext()
+        self.assertEqual(output, expectedoutput)
+
 if __name__ == "__main__":
     unittest.main()
