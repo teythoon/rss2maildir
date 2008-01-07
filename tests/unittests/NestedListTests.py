5af09d2ba2911d96735dd613f04cb92c8c6c1924
Brett Parker <iDunno@sommitrealweird.co.uk>
2008-01-07 01:00:44 +0000

    * unittest for mixture of different types of lists

--- tests/unittests/NestedListTests.py
+++ tests/unittests/NestedListTests.py
@@ -13,10 +13,14 @@ class NestedListTests(ParsingTests.ParsingTest):
     def testWellFormedNestedOrderedLists(self):
         return self.runParsingTest("nestedorderedlists-wellformed")
 
+    def testWellFormedMixedNestedLists(self):
+        return self.runParsingTest("mixednestedlists-wellformed")
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(NestedListTests("testWellFormedNestedLists"))
     suite.addTest(NestedListTests("testWellFormedNestedOrderedLists"))
+    suite.addTest(NestedListTests("testWellFormedMixedNestedLists"))
     return suite
 
 if __name__ == "__main__":
