c5841ddf4b7e8f896aab456826c6478a395dbb23
Justus Winter <4winter@informatik.uni-hamburg.de>
2011-09-22 21:19:52 +0200

    Fix error handling in mkdir_p

--- rss2maildir/utils.py
+++ rss2maildir/utils.py
@@ -23,7 +23,7 @@ def mkdir_p(path):
     try:
         os.makedirs(path)
     except OSError as e:
-        if e.errno == errno.EEXIST:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
             raise
