c5e2f0f0f4ea10899d7b18b7265e67735ab91219
Justus Winter <4winter@informatik.uni-hamburg.de>
2011-09-29 10:48:33 +0200

    Cleanups

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python
 
 import sys
-import unittest
 import os
+import glob
+import unittest
 
 basedir = os.path.realpath(os.path.dirname(__file__))
-unittestsdir = os.path.join( \
-    basedir, \
-    "unittests")
+unittests_module = 'unittests'
+unittests_path = os.path.join(basedir, unittests_module)
 
 sys.path.insert(0, os.path.join(basedir, '..'))
 
-unittestmodules = []
+unittestmodules = ['%s.%s' % (unittests_module, os.path.splitext(file_)[0])
+                   for file_ in glob.glob1(unittests_path, '*.py')
+                   if file_ != '__init__.py']
 
 fullsuite = unittest.TestSuite()
-
-# walk our directory tree looking for any modules available
-for root, dir, files in os.walk(unittestsdir):
-    for file in files:
-        if file[-3:] == ".py" and file != "__init__.py":
-            moduleinfo = ".".join(root[len(basedir)+1:].split(os.sep))
-            unittestmodules.append(".".join((moduleinfo, file[0:-3])))
 
 # run through the found modules and look for test suites
 for unittestmodule in unittestmodules:
     # try importing the test and getting the suite
     try:
-        suite_func = getattr(__import__(unittestmodule, {}, {}, ['']), "suite")
+        suite_func = getattr(__import__(unittestmodule, {}, {}, ['']), 'suite')
         # add the suite to the tests
-        fullsuite.addTest(suite_func())
     except:
         # there was not test suite in there to run, skip it
-        pass
+        continue
+    fullsuite.addTest(suite_func())
 
 testrunner = unittest.TextTestRunner()
 testrunner.run(fullsuite)
